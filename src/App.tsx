24e7272470efb2d8ba54fa0ac4ac7a3b7a1c939b
Aleksey Nogay <leshkanogay@gmail.com>
2023-11-15 19:45:35 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentOne } from "./components/Components"
+
+// function calc (a: number, b: number) {
+//   return Number(a + b)
+// }
+console.log('hello');
+const App: React.FC = () => {
+  // const result = calc(5,6)
+  return (
+    <ComponentOne />
+  )
+}
+
+export default App;
---
+++ src/components/Components.tsx
@@ -0,0 +1,183 @@
+import React from "react";
+
+// type StringOne = {
+//   textOne: string
+// }
+// type StringTwo = {
+//   textTwo: string
+// }
+// type StringThree = {
+//   textThree: string
+// }
+// type StringFour = {
+//   textFour: string
+// }
+// type StringFive = {
+//   textFive: string
+// }
+// type StringSix = {
+//   textSix: string
+// }
+// type StringSeven = {
+//   textSeven: string
+// }
+// type StringEight = {
+//   textEight: string
+// }
+// type StringNine = {
+//   textNine: string
+// }
+// type StringTen = {
+//   textTen: string
+// }
+
+// type AllString = StringOne & StringTwo & StringThree & StringFour & StringFive & StringSix & StringSeven & StringEight & StringNine & StringTen
+
+// type NumberOne = {
+//   numberOne: number
+// }
+// type NumberTwo = {
+//   numberTwo: number
+// }
+// type NumberThree = {
+//   numberThree: number
+// }
+// type NumberFour = {
+//   numberFour: number
+// }
+// type NumberFive = {
+//   numberFive: number
+// }
+// type NumberSix = {
+//   numberSix: number
+// }
+// type NumberSeven = {
+//   numberSeven: number
+// }
+// type NumberEight = {
+//   numberEight: number
+// }
+// type NumberNine = {
+//   numberNine: number
+// }
+// type NumberTen = {
+//   numberTen: number
+// }
+
+// type BooleanOne = {
+//   booleanOne: boolean
+// }
+// type BooleanTwo = {
+//   booleanTwo: boolean
+// }
+// type BooleanThree = {
+//   booleanThree: boolean
+// }
+// type BooleanFour = {
+//   booleanFour: boolean
+// }
+// type BooleanFive = {
+//   booleanFive: boolean
+// }
+// type BooleanSix = {
+//   booleanSix: boolean
+// }
+// type BooleanSeven = {
+//   booleanSeven: boolean
+// }
+// type BooleanEight = {
+//   booleanEight: boolean
+// }
+// type BooleanNine = {
+//   booleanNine: boolean
+// }
+// type BooleanTen = {
+//   booleanTen: boolean
+// }
+
+// type ObjOne = {
+//   number: number,
+//   string: string,
+// }
+// type ObjTwo = {
+//   number: number,
+//   string: string,
+// }
+// type ObjThree = {
+//   number: number,
+//   string: string,
+// }
+// type ObjFour = {
+//   number: number,
+//   string: string,
+// }
+// type ObjFive = {
+//   number: number,
+//   string: string,
+// }
+// type ObjSix = {
+//   number: number,
+//   string: string,
+// }
+// type ObjSeven = {
+//   number: number,
+//   string: string,
+// }
+// type ObjEight = {
+//   number: number,
+//   string: string,
+// }
+// type ObjNine = {
+//   number: number,
+//   string: string,
+// }
+// type ObjTen = {
+//   number: number,
+//   string: string,
+// }
+
+// const numbers: Array<number> = [1,2,3,4,5]
+// const letters: Array<string> = ['d','f','g','j','r']
+// const words: Array<string> = ['hello', 'bye']
+// const bigInt: Array<number> = [43523432,6453234,665432,6754]
+
+function calc (a: number, b: number) {
+  return Number(a + b)
+}
+
+calc(5,6)
+
+
+
+
+
+export const ComponentOne = (
+  // { textOne,
+  //   textTwo,
+  //   textThree,
+  //   textFour,
+  //   textFive,
+  //   textSix,
+  //   textSeven,
+  //   textEight,
+  //   textNine,
+  //   textTen
+  // }: AllString
+  ) => {
+  return ( 
+    <>
+      {/* <p>{textOne}</p>
+      <p>{textTwo}</p>
+      <p>{textThree}</p>
+      <p>{textFour}</p>
+      <p>{textFive}</p>
+      <p>{textSix}</p>
+      <p>{textSeven}</p>
+      <p>{textEight}</p>
+      <p>{textNine}</p>
+      <p>{textTen}</p> */}
+    </>
+  )
+}
+
+
